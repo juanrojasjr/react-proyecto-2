fe28bb6375de5cab05457e6743e322fcc24008cd
Juan Rojas <juandarojas01@gmail.com>
2023-09-25 23:18:04 -0500

    Project init React/Vite

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.scss'
+import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  )
+}
+
+export default App
